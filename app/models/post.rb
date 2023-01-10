354ad627462e696def566db8690be8848e0555fd
Allie Soldau <alliesoldau@gmail.com>
2023-01-10 11:30:05 -0500

    commit tree 3980d719719d7e0027fbe0013d2f5beee0c521bb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :title_cant_be_true_facts
+
+    def title_cant_be_true_facts
+        if title == "True Facts"
+            errors.add(:title, "Can't be True Facts")
+        end
+    end
 end
